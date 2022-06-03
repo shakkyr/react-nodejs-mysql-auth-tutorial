5d77bcb1980242faa70c8b7e706c42cc84b6743d
Shadi Rada <shakkyr@gmail.com>
2022-06-04 02:34:00 +0300

    add and get posts

--- server/index.js
+++ server/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 
 
+app.use(express.json())
+
 const db = require('./models')
 
+
+const postRouter = require('./routes/Posts.Routes')
+
+app.use('/posts', postRouter)
 
 db.sequelize.sync().then(()=> {
     app.listen(3001, () => {
